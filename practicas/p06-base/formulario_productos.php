05be5534a44555d73fe52df5ce211602004b0372
josueVJ <josuevj.1e.49@gmail.com>
2023-09-28 00:09:47 -0600

    ejercicio 1 y 2 de p06 terminados

---
+++ practicas/p06-base/formulario_productos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN” 
+“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>p1</title>
+</head>
+
+<body>
+    
+    <form method="post" action="set_producto_v2.php">
+        <label>Cliente</label><br>
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre"><br>
+        <label for="marca">Marca:</label>
+        <input type="text" name="marca"><br>
+        <label for="modelo">Modelo:</label>
+        <input type="text" name="modelo"><br>
+        <label for="precio">Precio:</label>
+        <input type="text" name="precio"><br>
+        <label for="detalles">Detalles:</label>
+        <input type="text" name="detalles"><br>
+        <label for="unidades">Unidades:</label>
+        <input type="text" name="unidades"><br>
+        <label for="imagen">Imagen:</label>
+        <input type="text" name="imagen"><br>
+        <input type="submit" name="registroProducto" value="Registrar">
+    </form>
+    
+</body>
+</html>
---
+++ practicas/p06-base/set_producto_v2.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN” 
+“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+<?php
+
+
+if (isset($_POST['registroProducto'])) {
+    @$link = new mysqli('localhost', 'root', 'Normita1230', 'marketzone');
+    if ($link->connect_errno) {
+        die('Falló la conexión: ' . $link->connect_error . '<br/>');
+    }
+
+    $marca = $_POST['marca'];
+    $modelo = $_POST['modelo'];
+    $precio = $_POST['precio'];
+    $detalles = $_POST['detalles'];
+    $unidades = $_POST['unidades'];
+    $imagen = $_POST['imagen'];
+
+    if (empty($nombre) || empty($marca) || empty($modelo) || empty($precio) || empty($detalles) || empty($unidades) || empty($imagen)) {
+        echo "Error: Todos los campos son obligatorios.";
+        echo '<a href="formulario_productos.php">Regresar</a>';
+    }
+    else{
+        if (!is_numeric($precio) || !preg_match('/^\d+(\.\d+)?$/', $precio)) {
+            echo "Error: El precio debe ser un número válido con punto decimal (por ejemplo, 10.99).";
+            echo '<a href="formulario_productos.php">Regresar</a>';
+        } 
+        elseif (!ctype_digit($unidades)) {
+            echo "Error: Las unidades deben ser un número entero.";
+            echo '<a href="formulario_productos.php">Regresar</a>';
+        }  
+        else{
+            $sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
+
+            if ($link->query($sql)) {
+                echo "Nuevo registro creado rey<br>";
+                echo 'Producto insertado con ID: ' . $link->insert_id . "<br>";
+                echo "Resumen de los datos insertados:<br>";
+                echo "Nombre: $nombre<br>";
+                echo "Marca: $marca<br>";
+                echo "Modelo: $modelo<br>";
+                echo "Precio: $precio<br>";
+                echo "Detalles: $detalles<br>";
+                echo "Unidades: $unidades<br>";
+                echo "Imagen: $imagen<br>";
+                echo '<a href="formulario_productos.php">Regresar</a>';
+            } else {
+                echo "Error al insertar en la tabla productos: " . $link->error;
+                echo '<a href="formulario_productos.php">Regresar</a>';
+            }
+            $link->close();
+        }
+    }
+}
+
+?>
+
+</body>
+</html>
