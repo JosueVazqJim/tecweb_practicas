fe38da44cc19293b5f1fe36f990bee1b47e3780b
josueVJ <josuevj.1e.49@gmail.com>
2023-09-28 20:14:06 -0600

    p06 terminada

---
+++ practicas/p06-base/get_productos_vigentes.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN” 
+“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<?php
+    $data = array();
+    if(isset($_GET['eliminado'])){
+        $eliminado = $_GET['eliminado'];
+
+    }
+
+    if ($eliminado == 1)
+    {
+        /** SE CREA EL OBJETO DE CONEXION */
+        @$link = new mysqli('localhost', 'root', 'Normita1230', 'marketzone');	
+
+        /** comprobar la conexión */
+        if ($link->connect_errno) 
+        {
+            die('Falló la conexión: '.$link->connect_error.'<br/>');
+                /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+        }
+
+        /** Crear una tabla que no devuelve un conjunto de resultados si es que esta el id*/
+        if ( $result = $link->query("SELECT * FROM productos WHERE eliminado = $eliminado") ) 
+        {
+            $row = $result->fetch_all(MYSQLI_ASSOC);
+            
+            $result->free();
+        }
+
+        $link->close();
+    }
+    elseif($eliminado == 0){
+        /** SE CREA EL OBJETO DE CONEXION */
+        @$link = new mysqli('localhost', 'root', 'Normita1230', 'marketzone');	
+
+        /** comprobar la conexión */
+        if ($link->connect_errno) 
+        {
+            die('Falló la conexión: '.$link->connect_error.'<br/>');
+                /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+        }
+
+        /** Crear una tabla que no devuelve un conjunto de resultados si es que esta el id*/
+        if ( $result = $link->query("SELECT * FROM productos WHERE eliminado = $eliminado") ) 
+        {
+            $row = $result->fetch_all(MYSQLI_ASSOC);
+            
+            $result->free();
+        }
+
+        $link->close();
+    }
+?>
+
+<body>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Marca</th>
+            <th scope="col">Modelo</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Unidades</th>
+            <th scope="col">Detalles</th>
+            <th scope="col">Imagen</th>
+            <th scope="col">eliminado</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <?php 
+                    foreach ($row as $registro) {
+                        echo '<tr>';
+                        echo '<td>' . $registro['id'] . '</td>';
+                        echo '<td>' . $registro['nombre'] . '</td>';
+                        echo '<td>' . $registro['unidades'] . '</td>';
+                        echo '<td>' . $registro['modelo'] . '</td>';
+                        echo '<td>' . $registro['precio'] . '</td>';
+                        echo '<td>' . $registro['unidades'] . '</td>';
+                        echo '<td>' . $registro['detalles'] . '</td>';
+                        echo '<td><img src="' . $registro['imagen'] . '"></td>';
+                        echo '<td>' . $registro['eliminado'] . '</td>';
+                    }
+                ?>
+                
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+	
+</html>
--- practicas/p06-base/set_producto_v2.php
+++ practicas/p06-base/set_producto_v2.php
@@ -25,40 +25,52 @@ if (isset($_POST['registroProducto'])) {
     $detalles = $_POST['detalles'];
     $unidades = $_POST['unidades'];
     $imagen = $_POST['imagen'];
-
+    
+    /* primero checa que no esten vacios los campos*/
     if (empty($nombre) || empty($marca) || empty($modelo) || empty($precio) || empty($detalles) || empty($unidades) || empty($imagen)) {
         echo "Error: Todos los campos son obligatorios.";
         echo '<a href="formulario_productos.php">Regresar</a>';
     }
     else{
+        /* is_numeric($precio) verifica si el valor de $precio es numérico 
+        !preg_match('/^\d+(\.\d+)?$/', $precio) analiza la expresion regularde $precio*/
         if (!is_numeric($precio) || !preg_match('/^\d+(\.\d+)?$/', $precio)) {
             echo "Error: El precio debe ser un número válido con punto decimal (por ejemplo, 10.99).";
             echo '<a href="formulario_productos.php">Regresar</a>';
         } 
+        /*comprueba que unidades es un entero*/
         elseif (!ctype_digit($unidades)) {
             echo "Error: Las unidades deben ser un número entero.";
             echo '<a href="formulario_productos.php">Regresar</a>';
         }  
         else{
-            $sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
+            // Validar longitud de campos nombre, marca, modelo, detalles e imagen de acuerdo a la base de datos
+            if (strlen($nombre) > 100 || strlen($marca) > 25 || strlen($modelo) > 25 || strlen($detalles) > 250 || strlen($imagen) > 100) {
+                echo "Error: Uno o más campos exceden la longitud permitida.";
+                echo '<a href="formulario_productos.php">Regresar</a>';
+            } 
+            else{
+                $sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', 0)";
 
-            if ($link->query($sql)) {
-                echo "Nuevo registro creado rey<br>";
-                echo 'Producto insertado con ID: ' . $link->insert_id . "<br>";
-                echo "Resumen de los datos insertados:<br>";
-                echo "Nombre: $nombre<br>";
-                echo "Marca: $marca<br>";
-                echo "Modelo: $modelo<br>";
-                echo "Precio: $precio<br>";
-                echo "Detalles: $detalles<br>";
-                echo "Unidades: $unidades<br>";
-                echo "Imagen: $imagen<br>";
-                echo '<a href="formulario_productos.php">Regresar</a>';
-            } else {
-                echo "Error al insertar en la tabla productos: " . $link->error;
-                echo '<a href="formulario_productos.php">Regresar</a>';
+                if ($link->query($sql)) {
+                    echo "Nuevo registro creado rey<br>";
+                    echo 'Producto insertado con ID: ' . $link->insert_id . "<br>";
+                    echo "Resumen de los datos insertados:<br>";
+                    echo "Nombre: $nombre<br>";
+                    echo "Marca: $marca<br>";
+                    echo "Modelo: $modelo<br>";
+                    echo "Precio: $precio<br>";
+                    echo "Detalles: $detalles<br>";
+                    echo "Unidades: $unidades<br>";
+                    echo "Imagen: $imagen<br>";
+                    echo '<a href="formulario_productos.php">Regresar</a>';
+                } else {
+                    echo "Error al insertar en la tabla productos: " . $link->error;
+                    echo '<a href="formulario_productos.php">Regresar</a>';
+                }
+                $link->close();
             }
-            $link->close();
+            
         }
     }
 }
